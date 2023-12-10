b15a550ddf798a7924acb38b447e1721b9c55e0b
Joshua Hyde <jrh3k5@gmail.com>
2023-12-09 21:34:20 -0600

    Add a dummy export

--- index.js
+++ index.js
@@ -68,6 +68,9 @@ searchIndex.addAll(documents);
 
 console.log("Search index construction complete");
 
+// do a dummy export just to make Vercel happy
+module.exports = {};
+
 run(async (context) => {
     const messageBody = context.message.content;
     await context.reply(`ECHO: ${messageBody}`);
